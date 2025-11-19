11a49e2575bb5cdd1ca79567fe50a73a05d3c1a0
Napsitoo <badis.haddadi30@gmail.com>
2025-11-19 17:32:38 +0100

    Add AOS initialization and navbar scroll effect

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+AOS.init({
+    duration: 800,
+    once: true
+});
+
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
